e8b44fe73ae2625158c00f46e16d86223701f256
peterlqs <nplongquan@gmail.com>
2021-10-07 16:41:39 +0700

    bug fixed

--- app/src/main/java/com/example/maple/ui/recommendation/MainRecommendation.kt
+++ app/src/main/java/com/example/maple/ui/recommendation/MainRecommendation.kt
@@ -1,17 +1,12 @@
 package com.example.maple.ui.recommendation
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import com.example.maple.R
+import androidx.fragment.app.Fragment
 import com.example.maple.databinding.MainRecommendationFragmentBinding
-import com.example.maple.databinding.MainScoreFragmentBinding
 import com.example.maple.ui.score.MainDialog
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainRecommendation : Fragment() {
 
@@ -21,21 +16,21 @@ class MainRecommendation : Fragment() {
     private lateinit var _binding: MainRecommendationFragmentBinding
     private val binding get() = _binding
 
-    val avatar = binding.userAva
-    val fabChangeAva = binding.fabAddAva
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.main_recommendation_fragment, container, false)
+    ): View {
+        _binding = MainRecommendationFragmentBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainRecommendationViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Do stuff here
+
+        val avatar = binding.userAva
+        val fabChangeAva = binding.fabAddAva
 
         fabChangeAva.setOnClickListener {
             val dialog = MainDialog()
