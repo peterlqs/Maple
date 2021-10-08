68b9809c370163057fbc9f68d4b3307ecf41be60
thaonhi1922 <vothaithaonhi0208@gmail.com>
2021-10-08 16:04:56 +0700

    add more text style in style.xml 30% main_setting_fragment.xml 90% main recommendation

--- app/src/main/java/com/example/maple/ui/recommendation/MainRecommendation.kt
+++ app/src/main/java/com/example/maple/ui/recommendation/MainRecommendation.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.maple.adapter.MainScoreAdapter
+import com.example.maple.adapter.RecommendationAdapter
 import com.example.maple.databinding.MainRecommendationFragmentBinding
 import com.example.maple.ui.score.MainDialog
 
@@ -27,17 +30,37 @@ class MainRecommendation : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // Do stuff here
-
+        // (Nhi's note for variables)
+        // avatar and fabChangeAva are used to change
+        // avatar in the main menu
         val avatar = binding.userAva
         val fabChangeAva = binding.fabAddAva
 
+        // (Nhi's justification for setOnClickListener)
+        // This code block assists in changing avatar.
+        // It is not completed right now due to the lack of graphic source.
         fabChangeAva.setOnClickListener {
             val dialog = MainDialog()
             dialog.show(childFragmentManager, "customDialog")
 
             //This part is for choosing avatar from our storage
         }
+
+        // (Nhi's justification for recyclerView)
+        // This code block is to use RecyclerView
+        // It is completed. Problems:
+            // - Being deprived of dataset
+            // - Not knowing to navigate
+            // - Not knowing how to display the screen in case of
+        // there is no subjects
+        val subjectView = binding.attentionSubject
+        subjectView.apply {
+            //Type grid, 2 item horizontally
+            layoutManager = GridLayoutManager(activity, 2)
+            //Set adapter, i cant explain the second parameter
+            //adapter = RecommendationAdapter(this)
+        }
+
     }
 
     //Creating cardview for recommendation
